5b924644e16ec8c1a5f8e173c309d10eb6485226
thirvan <thirvansomanah@gmail.com>
2021-06-24 02:52:43 +0400

    complete cash

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -7,13 +7,49 @@ int get_change(void);
 int main(void)
 {
     int cents;
+    int coins = 0;
+
     cents = get_change();
 
+    while (cents != 0)
+    {
+        // try to remove a quarter
+        if (cents >= 25)
+        {
+            cents -= 25;
+        }
+        // try to remove a dime
+        else if (cents >= 10)
+        {
+            cents -= 10;
+        }
+        // try to remove a nickle
+        else if (cents >= 5)
+        {
+            cents -= 5;
+        }
+        // try to remove a penny
+        else if (cents >= 1)
+        {
+            cents -= 1;
+        }
+        // if this else clause is executed there is a problem with the logic
+        else
+        {
+            printf("There is a bug with the logic\n");
+        }
 
-    printf("cents: %i\n", cents);
+        coins++;
+    }
+    
+
+
+    printf("%i\n", coins);
     return 0;
 }
 
+// prompt user for a non-negative amount of change in dollars and return that 
+// amount in cents
 int get_change(void)
 {
     float dollars;
@@ -22,8 +58,10 @@ int get_change(void)
     do
     {
         dollars = get_float("Changed owed: ");
+        // convert dollars to cents to the nearest penny
         cents = round(dollars * 100);
-    } while (dollars < 0);
+    }
+    while (dollars < 0);
     
 
 
