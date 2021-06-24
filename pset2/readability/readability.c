0ffa1fc77efcb245d934d44f786488f53854b66c
thirvan <thirvansomanah@gmail.com>
2021-06-25 03:35:39 +0400

    get num words

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -4,12 +4,13 @@
 #include <ctype.h>
 
 int count_letters(string text);
+int count_words(string text);
 
 int main(void)
 {
     string text = get_string("Text: ");
     printf("%i letter(s)\n", count_letters(text));
-    // printf("%s\n", text);
+    printf("%i word(s)\n", count_words(text));
 }
 
 // return the amount of alphabetical characters from text
@@ -26,3 +27,21 @@ int count_letters(string text)
 
     return count;
 }
+
+// return the amount of words separated with a space in text
+int count_words(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++ )
+    {
+        if (text[i] == ' ')
+        {
+            count++;
+        }
+    }
+    // increment count one extra time for the last word
+    count++;
+
+    return count;
+}
+
