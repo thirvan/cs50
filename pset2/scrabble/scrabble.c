0d4c757032b4db0f23c0d40d79891fae5aaee298
thirvan <thirvansomanah@gmail.com>
2021-06-24 22:35:27 +0400

    complete scrabble

--- pset2/scrabble/scrabble.c
+++ pset2/scrabble/scrabble.c
@@ -7,6 +7,7 @@
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int compute_score(string word);
+int get_letter_point(char ch);
 
 int main(void)
 {
@@ -18,10 +19,49 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    // Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 int compute_score(string word)
 {
-    // TODO: Compute and return score for string
+    int score = 0;
+
+    // Compute and return score for string
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        score += get_letter_point(word[i]);
+    }
+
+    return score;
+}
+
+// return the point associated with a letter
+int get_letter_point(char ch)
+{
+    int point = 0;
+
+    if (isalpha(ch))
+    {
+        int letter_index = toupper(ch) - 65;
+        point = POINTS[letter_index];
+    }
+    // point is 0 if character is not a letter
+    else
+    {
+        point = 0;
+    }
+
+    return point;
 }
