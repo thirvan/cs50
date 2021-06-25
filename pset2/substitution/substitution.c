e1efdc32efa1674149815a3c398ad5226936d51f
thirvan <thirvansomanah@gmail.com>
2021-06-25 16:15:21 +0400

    complete week 2

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void cipher(string text, int k);
+void encipher(string text, string k);
 void usage(void);
 bool validate_key(string key);
 
@@ -15,15 +15,13 @@ int main(int argc, string argv[])
     if (argc == 2)
     {
         string key = argv[1];
-        // Array of booleans for each alphabet, becomes true when found in key
 
 
         if (validate_key(key) == true)
         {
-            printf("valid key: %s\n", key);
-            // string text = get_string("plaintext: ");
-            // cipher(text, k);
-            // printf("ciphertext: %s\n", text);
+            string text = get_string("plaintext: ");
+            encipher(text, key);
+            printf("ciphertext: %s\n", text);
             return 0;
         }
         else
@@ -38,45 +36,47 @@ int main(int argc, string argv[])
     }
 }
 
-// Encrypt text with the key k
-void cipher(string text, int k)
+// Encipher text with key 
+void encipher(string text, string key)
 {
-    // Get number of shift to be between 0 and 25
-    int offset = k % 26;
 
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        int plain_ch = (int) text[i];
-
+        int plain_ch = text[i];
         int cipher_ch = plain_ch;
-        // If an alphabet, shift by k letters
+
         if (isalpha(plain_ch))
         {
-            cipher_ch += offset;
-
-            // If letter exceeds range of lower or uppercase, wrap back to 
-            // correct letter
-            if ((islower(plain_ch) && cipher_ch > 'z') || (isupper(plain_ch) && cipher_ch > 'Z'))
+            int alpha_idx;
+            // If is an uppercase character substitute with corresponding 
+            // uppercase character from key
+            if (isupper(plain_ch))
             {
-                cipher_ch -= 'Z' - 'A' + 1;
+                alpha_idx = plain_ch - 'A';
+                cipher_ch = toupper(key[alpha_idx]);
+            }
+            // If is a lowercase character substitute with corresponding 
+            // lowercase character from key
+            else
+            {
+                alpha_idx = plain_ch - 'a';
+                cipher_ch = tolower(key[alpha_idx]);
             }
         }
 
-        text[i] = (char) cipher_ch;
+        text[i] = cipher_ch;
     }
 }
 
-// Return the key as an int if it is a positive int, else return -1
+// Return true if the key is valid else return false
 bool validate_key(string key)
 {
     bool valid = true;
-    int k = -1;
+    // Array of booleans for each alphabet, becomes true when found in key
     bool alphabets[26] = {false};
-
     // Validate length of key
     if (strlen(key) == 26)
     {
-
         for (int i = 0; i < 26 && valid == true; i++)
         {
             char ch = key[i];
@@ -86,7 +86,6 @@ bool validate_key(string key)
                 ch = toupper(ch);
                 // Get index of character in bool array
                 int alpha_idx = ch - 'A';
-
                 // New character
                 if (alphabets[alpha_idx] == false)
                 {
@@ -98,7 +97,6 @@ bool validate_key(string key)
                     valid = false;
                     printf("Key must not contain repeated characters.\n");
                 }
-
             }
             else
             {
@@ -106,7 +104,6 @@ bool validate_key(string key)
                 printf("Key must only contain alphabetic characters.\n");
             }
         }
-
     }
     else
     {
