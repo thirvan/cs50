47e5a62dc3e47ed1c281ad90a1dd49802285d4db
thirvan <thirvansomanah@gmail.com>
2021-06-29 23:52:30 +0400

    complete grayscale

--- pset4/less/filter/helpers.c
+++ pset4/less/filter/helpers.c
@@ -1,8 +1,22 @@
+#include <math.h>
 #include "helpers.h"
+
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float sum = image[i][j].rgbtBlue + image[i][j].rgbtGreen + 
+                    image[i][j].rgbtRed;
+            int average = round(sum / 3);
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen= average;
+            image[i][j].rgbtRed= average;
+        }
+    }
     return;
 }
 
