f39e5328164edc8c5eaff1f33c50da99253a872a
thirvan <thirvansomanah@gmail.com>
2021-07-05 21:20:42 +0400

    complete lab6

--- lab6/tournament.py
+++ lab6/tournament.py
@@ -16,9 +16,21 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
+    with open(sys.argv[1], "r") as file:
+        reader = csv.DictReader(file)
+        for team in reader:
+            team["rating"] = int(team["rating"])
+            # print(team)
+            teams.append(team)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
+    for i in range(N):
+        winner = simulate_tournament(teams)
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] = 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
@@ -50,6 +62,11 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
+    while True:
+        teams = simulate_round(teams)
+        if len(teams) == 1:
+            break
+    return teams[0]["team"]
 
 
 if __name__ == "__main__":
