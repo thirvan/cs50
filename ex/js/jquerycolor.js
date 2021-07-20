04af5e4fa5ef92d75550d82a22ea41d262f7d577
thirvan <thirvansomanah@gmail.com>
2021-07-20 20:34:59 +0400

    try jquery

---
+++ ex/js/jquerycolor.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.jQButton').click(function() {
+        $('#colorDiv').css('background-color', 'black');
+        // $('#colorDiv').css('background-color', this.innerHTML.toLowerCase());
+    });
+});
