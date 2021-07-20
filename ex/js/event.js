889c490b3072918e7bace6ba30c138cb86c31cd2
thirvan <thirvansomanah@gmail.com>
2021-07-20 19:01:40 +0400

    make color change with event

---
+++ ex/js/event.js
@@ -0,0 +1,6 @@
+function changeColorEvent(event)
+{
+    var triggerObject = event.srcElement;
+    document.getElementById('colorDiv').style.backgroundColor = triggerObject.innerHTML.toLowerCase();
+
+}
