63545228cacc0f7381e87748b4f880a22eff42c2
thirvan <thirvansomanah@gmail.com>
2021-07-20 21:29:17 +0400

    include jquery library

--- ex/js/jquerycolor.js
+++ ex/js/jquerycolor.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $('.jQButton').click(function() {
-        $('#colorDiv').css('background-color', 'black');
-        // $('#colorDiv').css('background-color', this.innerHTML.toLowerCase());
+        $('#colorDiv').css('background-color', this.innerHTML.toLowerCase());
     });
 });
